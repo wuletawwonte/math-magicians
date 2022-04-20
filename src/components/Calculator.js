de32ae3aa28cdad4c37a6f3612ca3afdeed74bf1
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-20 15:55:16 +0300

    Remove poilerplate code and Add Calculator Component

---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>This is Calculator</h1>
+    );
+  }
+}
+
+export default Calculator;
