ab683f6ee20e3ef767baf537eae05f536a714635
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-20 16:36:51 +0300

    Style the calculator to resemble the design

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,26 +10,26 @@ class Calculator extends React.Component {
   render() {
     return (
       <div className="calculator">
-        <input type="text" className="output c-item" />
+        <input type="text" className="output c-item" value="0" />
         <button type="button" className="c-item">AC</button>
         <button type="button" className="c-item">+/-</button>
         <button type="button" className="c-item">%</button>
-        <button type="button" className="c-item">+</button>
+        <button type="button" className="c-item bg-orange">+</button>
         <button type="button" className="c-item">7</button>
         <button type="button" className="c-item">8</button>
         <button type="button" className="c-item">9</button>
-        <button type="button" className="c-item">x</button>
+        <button type="button" className="c-item bg-orange">x</button>
         <button type="button" className="c-item">4</button>
         <button type="button" className="c-item">5</button>
         <button type="button" className="c-item">6</button>
-        <button type="button" className="c-item">-</button>
+        <button type="button" className="c-item bg-orange">-</button>
         <button type="button" className="c-item">1</button>
         <button type="button" className="c-item">2</button>
         <button type="button" className="c-item">3</button>
-        <button type="button" className="c-item">+</button>
+        <button type="button" className="c-item bg-orange">+</button>
         <button type="button" className="c-item zero">0</button>
         <button type="button" className="c-item">.</button>
-        <button type="button" className="c-item">=</button>
+        <button type="button" className="c-item bg-orange">=</button>
       </div>
     );
   }
