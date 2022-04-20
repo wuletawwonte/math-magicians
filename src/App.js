c9fa901e69218381e82e5cd44d4c70cd5423dac6
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-20 17:14:37 +0300

    Remove all CRA boilerplate files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="main-container">
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="main-container">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
