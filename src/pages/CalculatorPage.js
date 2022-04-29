622ca31c34b151ea197aba60364b874bfb3cd296
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-29 19:36:40 +0300

    Add Inline style for the CalculatorPage component

--- src/pages/CalculatorPage.js
+++ src/pages/CalculatorPage.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Calculator from '../components/Calculator';
 
 const CalculatorPage = () => (
-  <section className="calculator-page">
+  <section className="calculator-page" style={{ display: 'flex', justifyContent: 'space-between' }}>
+    <h2>Lets Do some math!</h2>
     <Calculator />
   </section>
 );
