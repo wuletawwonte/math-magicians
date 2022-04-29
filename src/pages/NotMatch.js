5eb8d0acbe2500ffd4ee10018e78b8bc85ce178c
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-04-29 19:39:39 +0300

    Add global styling for the not-match class name

--- src/pages/NotMatch.js
+++ src/pages/NotMatch.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const NotMatch = () => (
-  <div>
+  <section className="not-match">
     <h3>No match for this page</h3>
-  </div>
+  </section>
 );
 export default NotMatch;
